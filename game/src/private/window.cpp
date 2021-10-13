34fce342dd539e7fb692811f18bbf073e69cf1ca
Colin Kennedy <colinvfx@gmail.com>
2021-10-12 20:17:03 -0700

    Got a OpenGL triangle to render on-screen

--- game/src/private/window.cpp
+++ game/src/private/window.cpp
@@ -10,5 +10,29 @@ namespace game {
         // Reference: https://youtu.be/yDgbNeYmG7g?t=605
         //
         assert(error_code == 0);
+
+        glGenBuffers(1, &this->vertex_buffer_id);
+        glBindBuffer(GL_ARRAY_BUFFER, this->vertex_buffer_id);
+
+        float vertices[] = {
+            +0.0f, +0.1,
+            -0.1f, -0.1f,
+            +0.1f, -0.1f,
+        };
+
+        glBufferData(
+            GL_ARRAY_BUFFER,
+            sizeof(vertices),
+            vertices,
+            GL_STATIC_DRAW
+        );
+    }
+
+    void Window::paintGL()
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
     }
 }
--- game/src/private/window.h
+++ game/src/private/window.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <QtOpenGL/QGLWidget>
+#include <QtOpenGL/qgl.h>  // Needed for `GLuint`
+
 
 namespace game {
     class Window : public QGLWidget {
+        private:
+            GLuint vertex_buffer_id;
+
         protected:
             virtual void initializeGL();
+            virtual void paintGL();
     };
 };
