f4c70d652b0fa81590c469e16326a03ff3fdcf24
Colin Kennedy <colinvfx@gmail.com>
2021-10-16 19:12:01 -0700

    Added missing default destructor

--- engine/src/include/vector2D.h
+++ engine/src/include/vector2D.h
@@ -8,6 +8,7 @@ namespace engine {
         public:
             Vector2D() : x(0.0f), y(0.0f) {};
             Vector2D(float x, float y) : x(x), y(y) {};
+            ~Vector2D() = default;
 
             float x;
             float y;
