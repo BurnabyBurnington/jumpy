0f06d335672fd987d159457b104e129245029d67
Colin Kennedy <colinvfx@gmail.com>
2021-10-30 00:17:40 -0700

    Got tests to pass

--- engine/src/include/matrix2D.h
+++ engine/src/include/matrix2D.h
@@ -10,6 +10,8 @@ namespace engine
             explicit Matrix2D(float r0c0, float r0c1, float r1c0, float r1c1) : r0c0(r0c0), r0c1(r0c1), r1c0(r1c0), r1c1(r1c1) {};
             ~Matrix2D() = default;
 
+            friend Vector2D operator*(Matrix2D const &left, Vector2D const &right);
+
             // TODO: Make the indices private, later
             // TODO: Check later if a members list produces less instructions / is faster
             float r0c0;
