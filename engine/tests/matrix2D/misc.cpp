81511957aa8f67d41e85412c7758287b30cd1076
Colin Kennedy <colinvfx@gmail.com>
2021-10-31 15:10:45 -0700

    Added an extra, missing test

--- engine/tests/matrix2D/misc.cpp
+++ engine/tests/matrix2D/misc.cpp
@@ -1,7 +1,7 @@
 #include <ctest.h>
 #include <matrix2D.h>
 
-CTEST(matrixBasics, initialization)
+CTEST(matrixBasics, default)
 {
     math::Matrix2D matrix {};
 
@@ -9,4 +9,14 @@ CTEST(matrixBasics, initialization)
     ASSERT_EQUAL(0.0f, matrix.r1c0);
     ASSERT_EQUAL(1.0f, matrix.r0c0);
     ASSERT_EQUAL(1.0f, matrix.r1c1);
+}
+
+CTEST(matrixBasics, initialization)
+{
+    math::Matrix2D matrix {1, 2, 3, 4};
+
+    ASSERT_EQUAL(1.0f, matrix.r0c1);
+    ASSERT_EQUAL(2.0f, matrix.r1c0);
+    ASSERT_EQUAL(3.0f, matrix.r0c0);
+    ASSERT_EQUAL(4.0f, matrix.r1c1);
 }
