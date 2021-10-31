96698c57cf0b98a2702b0e872390b642be6aefb8
Colin Kennedy <colinvfx@gmail.com>
2021-10-31 09:23:27 -0700

    Added more tests

--- engine/tests/matrix2D/rotate.cpp
+++ engine/tests/matrix2D/rotate.cpp
@@ -6,20 +6,19 @@
 
 // TODO: Add tests with a huge float value
 // TODO: Add variations
+// TODO: Make easier to read
 
 auto const SQUARE_ROOT_2_OVER_2 {0.70710678118f};
 auto const SQUARE_ROOT_3_OVER_2 {0.86602540378f};
 
-CTEST(matrixRotate, zero) {
-    auto matrix = math::Matrix2D::rotate(math::Radian{0.0});
-
-    ASSERT_FLOAT_EQUAL(1, matrix.r0c0);
-    ASSERT_FLOAT_EQUAL(0, matrix.r0c1);
-    ASSERT_FLOAT_EQUAL(0, matrix.r1c0);
-    ASSERT_FLOAT_EQUAL(1, matrix.r1c1);
+CTEST(matrixRotate, const) {
+    auto const matrix1 = math::Matrix2D::rotate(math::Radian{M_PI});
+    ASSERT_FLOAT_EQUAL(-1, matrix1.r0c0);
+    ASSERT_FLOAT_EQUAL(0, matrix1.r0c1);
+    ASSERT_FLOAT_EQUAL(0, matrix1.r1c0);
+    ASSERT_FLOAT_EQUAL(-1, matrix1.r1c1);
 }
 
-// TODO: Make easier to read
 CTEST(matrixRotate, pi) {
     auto matrix1 = math::Matrix2D::rotate(math::Radian{M_PI});
     ASSERT_FLOAT_EQUAL(-1, matrix1.r0c0);
@@ -58,3 +57,11 @@ CTEST(matrixRotate, pi) {
     ASSERT_FLOAT_EQUAL(SQUARE_ROOT_2_OVER_2, matrix6.r1c1);
 }
 
+CTEST(matrixRotate, zero) {
+    auto matrix = math::Matrix2D::rotate(math::Radian{0.0});
+
+    ASSERT_FLOAT_EQUAL(1, matrix.r0c0);
+    ASSERT_FLOAT_EQUAL(0, matrix.r0c1);
+    ASSERT_FLOAT_EQUAL(0, matrix.r1c0);
+    ASSERT_FLOAT_EQUAL(1, matrix.r1c1);
+}
